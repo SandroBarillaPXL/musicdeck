7815e8366ba6f7844b452eec13db3881cace88a0
sandropxl <Sandrobarilla2001@gmail.com>
2025-05-06 14:10:14 +0200

    tweak rfid r/w

--- read_rfid.py
+++ read_rfid.py
@@ -3,10 +3,15 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-try:    
-        print("Now place your tag to read")
-        id, text = reader.read()
+try:
+    id = reader.read_id_no_block()
+    if id is None:
+        # No card detected
+        print("")
+        print("")
+    else:
+        _, text = reader.read()  # This still blocks, but only if a card is present
         print(id)
-        print(text)
+        print(text.strip())  # Strip newline or trailing spaces
 finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
--- write_rfid.py
+++ write_rfid.py
@@ -4,8 +4,7 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        uri = "spotify:album:3DQueEd1Ft9PHWgovDzPKh" # Fred again.. - ten days
-        print("Now place your tag to write")
+        uri = input("Enter the URI to write to the tag: ")
         reader.write(uri)
         print("Written")
 finally:
